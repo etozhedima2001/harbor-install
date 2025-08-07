06e15929d7e6fb1c18754b07b033a7935370a963
etozhedima <whydmitriy@gmail.com>
2025-08-07 19:26:51 +0900

    add some fun moments

--- ansible/set_inventory.sh
+++ ansible/set_inventory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "harbor-server ansible_host=$(cat ../vm-create-terraform/harbor_server_ip.txt) ansible_user=root ansible_connection=ssh" > inventory.yml
-echo "ca-server ansible_host=$(cat ../vm-create-terraform/ca_server_ip.txt) ansible_user=root ansible_connection=ssh" >> inventory.yml
+echo "harbor-server ansible_host=$(cat ../vm-create-terraform/server_ip/harbor_server_ip.txt) ansible_user=root ansible_connection=ssh" > inventory.yml
+echo "ca-server ansible_host=$(cat ../vm-create-terraform/server_ip/ca_server_ip.txt) ansible_user=root ansible_connection=ssh" >> inventory.yml
--- ansible/start.sh
+++ ansible/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set_inventory.sh
-ansible-playbook playbook/set_config.yml
+ansible-playbook playbooks/set_config.yml
 echo "remote_port = 1871" >> ansible.cfg
-ansible-playbook playbook/install_ca.yaml
+ansible-playbook playbooks/install_ca.yaml
